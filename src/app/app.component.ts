e89cfe72e98bce7969261251cb68cf4638ccaea5
JacksonSmith43 <jessica444@gmx.at>
2025-06-16 11:10:48 +0200

    211. Converting Signals To Observables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, DestroyRef, OnInit, inject, signal, effect } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -10,23 +11,18 @@ import { interval, map } from 'rxjs';
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   clickCount = signal(0);
-
-  constructor() {
-    effect(() => {
-      console.log(`Clicked button ${this.clickCount()} times.`);
-    })
-  }
+  clickCount$ = toObservable(this.clickCount);
 
   ngOnInit(): void {
-    const subscription = interval(1000).pipe(
-      map((val) => val * 2)
-    ).subscribe({
-      next: (val) => console.log(val)
+    const subscription = this.clickCount$.subscribe({
+      next: (val) =>
+        console.log(`Clicked button ${this.clickCount()} times.`)
     });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     })
+
   }
 
 
